d5d1e82dc5f72cad8ae866c8aa700ef825b2ec7e
Jens Hilligsøe <github@hilli.dk>
2024-02-25 14:59:39 +0100

    Brew formula update for go-kef-w2 version v0.0.2

--- Formula/kefw2.rb
+++ Formula/kefw2.rb
@@ -9,9 +9,9 @@ class Kefw2 < Formula
   license "MIT"
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/hilli/go-kef-w2/releases/download/v0.0.2/go-kef-w2_Darwin_arm64.tar.gz"
-      sha256 "e8de1c27cbd548e94ba578b7fe64c67910a4c194356370585562522980355911"
+    if Hardware::CPU.intel?
+      url "https://github.com/hilli/go-kef-w2/releases/download/v0.0.2/go-kef-w2_Darwin_x86_64.tar.gz"
+      sha256 "8b3dca23a5e1fecda71ce1e14276eef7816aefd05eb25a23575e5085628e0e6d"
 
       def install
         bin.install "kefw2"
@@ -20,9 +20,9 @@ class Kefw2 < Formula
         fish_completion.install "completions/kefw2.fish"
       end
     end
-    if Hardware::CPU.intel?
-      url "https://github.com/hilli/go-kef-w2/releases/download/v0.0.2/go-kef-w2_Darwin_x86_64.tar.gz"
-      sha256 "b0495f0dfd6d762a748c536ccad9031b2df9a48d33bfa9bb2b07e604b16da501"
+    if Hardware::CPU.arm?
+      url "https://github.com/hilli/go-kef-w2/releases/download/v0.0.2/go-kef-w2_Darwin_arm64.tar.gz"
+      sha256 "679705562eecb1baf483fbf1106015cbcb224988e187085471960a1a9b876106"
 
       def install
         bin.install "kefw2"
@@ -36,7 +36,7 @@ class Kefw2 < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/hilli/go-kef-w2/releases/download/v0.0.2/go-kef-w2_Linux_x86_64.tar.gz"
-      sha256 "a1f08e55643dba3df0776928de49405ff906c53762487f6c504c656a470dd47c"
+      sha256 "35be4ff861146ca76c967e27f0efca2f9be1f6e3313b39b7d900a42d988b60d3"
 
       def install
         bin.install "kefw2"
@@ -47,7 +47,7 @@ class Kefw2 < Formula
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/hilli/go-kef-w2/releases/download/v0.0.2/go-kef-w2_Linux_arm64.tar.gz"
-      sha256 "87f939baa9c5dc6857c4e2b42648465fa111cec9df38e3e2f354e77d59ccaa96"
+      sha256 "4871a02a6c18e66a6652c161c023901b4ccb2875bf9b32690010c720d19b3b30"
 
       def install
         bin.install "kefw2"
